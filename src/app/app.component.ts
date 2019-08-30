5dcc65e0c163234f52085a454643d2344b52d224
Apple <siddharth@nisostech.com>
2019-08-30 19:45:17 +0530

    subscribe SwUpdate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pwa-demo';
+
+  update: boolean;
+
+  constructor(public updates: SwUpdate) {
+
+    updates.available.subscribe( () => {
+      this.update = true;
+    });
+  }
 }
